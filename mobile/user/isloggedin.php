c27c6478f75a6dfb419d0e47c15ba6dea527c358
Mayank kumar <mayankmk165@gmail.com>
2019-03-25 13:17:26 +0530

    Completed Login API
    Login Rest API is completed.

--- mobile/user/isloggedin.php
+++ mobile/user/isloggedin.php
@@ -14,41 +14,39 @@
                 //Check if the user login_status is true or false for the folling user_id in the Users table.
                 $query = "SELECT login_status FROM users WHERE  id = '$id'";
                 $result = mysqli_query($con,$query);
+               
                 if (mysqli_num_rows($result)>0) {
-                    //User exist with this user_id
-                    if (mysqli_num_rows($result)>0) {
-                        //User is Valid
-                        $dataResult = array();
-                        while ($row = mysqli_fetch_row($result)) {
-                            $dataResult = $row;
-                        }
-                        $loginStatus = $dataResult[0];
-                        if ($loginStatus == 1) {
-                            $logged_in = true;
-                            $data['error'] = false;
-                            $data['logged_in'] = true;
-                        } else {
-                            $logged_in = false;
-                            $data['error'] = false;
-                            $data['logged_in'] = false;
-                        }
+                    //User is Valid
+                    $dataResult = array();
+                    while ($row = mysqli_fetch_row($result)) {
+                        $dataResult = $row;
+                    }
+                    $loginStatus = $dataResult[0];
+                    if ($loginStatus == 1) {
+                        $logged_in = true;
+                        $data['error'] = false;
+                        $data['logged_in'] = true;
                     } else {
-                        //Invalid UserID
-                        $data['error'] = true;
-                        $data['message'] = 'Invalid UserID';
+                        $logged_in = false;
+                        $data['error'] = false;
+                        $data['logged_in'] = false;
                     }
                 } else {
+                    //Invalid UserID
                     $data['error'] = true;
-                    $data['message'] = 'Required Data Missing!';
+                    $data['message'] = 'Invalid UserID';
                 }
             } else {
                 $data['error'] = true;
-                $data['message'] = 'Invalid Request'; 
+                $data['message'] = 'Required Data Missing!';
             }
         } else {
-            $data['error']=true;
-            $data['message']="Server Error, Try Again Later";
+            $data['error'] = true;
+            $data['message'] = 'Invalid Request'; 
         }
+    } else {
+        $data['error']=true;
+        $data['message']="Server Error, Try Again Later";
     }
 
     echo json_encode($data);
--- mobile/user/login.php
+++ mobile/user/login.php
@@ -3,50 +3,70 @@
 
     $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 
+    $data = array();
+
     if ($con) {
-        $email = $_REQUEST['email'];
-        $password = $_REQUEST['password'];
+        if ($_SERVER['REQUEST_METHOD']=='POST') {
+            if (isset($_REQUEST['email']) and isset($_REQUEST['password'])) {
+                $email = $_REQUEST['email'];
+                $password = $_REQUEST['password'];
 
-        //Lets covert password to md5
-        $md5pass = md5($password);
+                //Lets covert password to md5
+                $md5pass = md5($password);
 
-        //Now need to check if the user exist in database or not.
-        $searchEmailQuery = "SELECT * from users WHERE email='$email'";
-        $result = mysqli_query($con,$searchEmailQuery);
-        if (mysqli_num_rows($result)>0) {
-            //User exist with this Email ID
-            //Now will check for the password value if it matches particular value or not.
-            $passMatchQuery = "SELECT email,username,firstname,lastname,dob,mobile,gender FROM users WHERE email='$email' AND pass='$md5pass'";
-            $passResult = mysqli_query($con,$passMatchQuery);
-            if (mysqli_num_rows($passResult)>0) {
-                //User is Valid, Login Success!
-                $data = array();
-                while ($row = mysqli_fetch_row($passResult)) {
-                    $data = $row;
+                //Now need to check if the user exist in database or not.
+                $searchEmailQuery = "SELECT * from users WHERE email='$email'";
+                $result = mysqli_query($con,$searchEmailQuery);
+                if (mysqli_num_rows($result)>0) {
+                    //User exist with this Email ID
+                    //Now will check for the password value if it matches particular value or not.
+                    $passMatchQuery = "SELECT email,username,fullname,dob,mobile,gender,id FROM users WHERE email='$email' AND pass='$md5pass'";
+                    $passResult = mysqli_query($con,$passMatchQuery);
+                    if (mysqli_num_rows($passResult)>0) {
+                        //User is Valid, Login Success!
+                        $dataOutput = array();
+                        while ($row = mysqli_fetch_row($passResult)) {
+                            $dataOutput = $row;
+                        }
+                        $emailValue = $dataOutput[0];
+                        $username = $dataOutput[1];
+                        $fullname = $dataOutput[2];
+                        $dob = $dataOutput[3];
+                        $mobile = $dataOutput[4];
+                        $gender =$dataOutput[5];
+                        $id = $dataOutput[6];
+
+                        $data['status']='success';
+                        $data['email']=$emailValue;
+                        $data['username']=$username;
+                        $data['fullname']=$fullname;
+                        $data['dob']=$dob;
+                        $data['mobile']=$mobile;
+                        $data['gender']=$gender;
+                        $data['userid']=$id;
+                    } else {
+                        //Invalid Password
+                        $data['error']=true;
+                        $data['message']='Invalid Password';  
+                    }
+                } else {
+                    $data['error']=true;
+                    $data['message']='Email Id does not exist!';
                 }
-                $emailValue = $data[0];
-                $username = $data[1];
-                $firstname = $data[2];
-                $lastname = $data[3];
-                $dob = $data[4];
-                $mobile = $data[5];
-                $gender =$data[6];
-
-                $finalOutput = ['status'=>'success','email'=>$emailValue,'username'=>$username,'fname'=>$firstname,'lname'=>$lastname,'dob'=>$dob,'mobile'=>$mobile,'gender'=>$gender];
-                echo json_encode($finalOutput);
             } else {
-                //Invalid Password
-                $data = ['message'=>'Invalid Password'];
-                echo json_encode($data);
+                $data['error'] = true;
+                $data['message'] = 'Required Data Missing!';
             }
         } else {
-            //User does not exist
-            $data = ['message'=>'User does not exist'];
-            echo json_encode($data);
+            $data['error'] = true;
+            $data['message'] = 'Invalid Request'; 
         }
     } else {
-        //Connectivity Error
-        $data = ['message'=>'Connection Error'];
-        echo json_encode($data);
+        $data['error']=true;
+        $data['message']="Error! Try Again After Sometime";
     }
+    
+    echo json_encode($data);
+
+    mysqli_close($con);
 ?>
